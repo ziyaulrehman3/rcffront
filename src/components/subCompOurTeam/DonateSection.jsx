2d9f200158dcb7779eb78582948ea1272e942b77
ziyaulrehman3 <ziyaulrehman3@gmail.com>
2025-03-05 02:34:13 +0530

    semi final 4.5

--- src/components/subCompOurTeam/DonateSection.jsx
+++ src/components/subCompOurTeam/DonateSection.jsx
@@ -7,7 +7,7 @@ const DonateSection = () => {
       <p className="text-lg text-[#787878] font-poppins mb-8">
         Your generosity fuels our mission to help those in need. Join us in making a lasting impact by donating today.
       </p>
-      <Link to="/Donate"  className="bg-[#034C5B] text-white font-poppins px-6 py-4 rounded-[40px] hover:bg-[#126e80] transition-all">
+      <Link to="/Donation"  className="bg-[#034C5B] text-white font-poppins px-6 py-4 rounded-[40px] hover:bg-[#126e80] transition-all">
         Donate Now
       </Link>
     </div>
--- src/components/subCompOurTeam/Header.jsx
+++ src/components/subCompOurTeam/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="text-center my-[5%] w-full">
-      <h2 className="text-4xl font-shippori font-semibold tracking-[0.05em]">Meet Our Team</h2>
-      <p className="text-[#E3501F] text-xl font-poppins font-semibold mt-2 pt-4 pb-4">Passionate Minds, United for a Cause</p>
-      <p className="text-[#787878] lg:text-xl gap-0 font-poppins max-w-[1000px] h-[90px] mx-auto flex items-center justify-center text-center w-11/12">
+      <h2 className="text-2xl font-shippori font-regular tracking-[0.05em]">Meet Our Team</h2>
+      <p className="text-[#E3501F] text-2xl font-poppins font-regular mt-2 pt-3 pb-3">Passionate Minds, United for a Cause</p>
+      <p className="text-[#9CA3AF] lg:text-xl gap-0 font-poppins max-w-[1000px] h-[90px] mx-auto flex items-center justify-center text-center w-11/12">
         Our dedicated team works tirelessly to create a positive impact on society. Each member brings unique skills, commitment, and a shared vision to drive meaningful change.
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 "></div>
