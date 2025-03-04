6f979dcfe8ff7e7a8cd2565e089b6e4729425897
ziyaulrehman3 <ziyaulrehman3@gmail.com>
2025-03-05 03:01:22 +0530

    semi final 4.5

--- src/components/subCompOurTeam/Header.jsx
+++ src/components/subCompOurTeam/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="text-center my-[5%] w-full">
-      <h2 className="text-2xl font-shippori font-regular tracking-[0.05em]">Meet Our Team</h2>
-      <p className="text-[#E3501F] text-2xl font-poppins font-regular mt-2 pt-3 pb-3">Passionate Minds, United for a Cause</p>
-      <p className="text-[#9CA3AF] lg:text-xl gap-0 font-poppins max-w-[1000px] h-[90px] mx-auto flex items-center justify-center text-center w-11/12">
+      <h2 className="text-xl font-shippori  tracking-[0.05em]">Meet Our Team</h2>
+      <p className="text-[#E3501F] text-xl font-poppins font-normal mt-2 py-2 px-2">Passionate Minds, United for a Cause</p>
+      <p className="text-[#9CA3AF]  gap-0 font-poppins w-[85%]  h-[75px] mx-auto flex items-center justify-center text-base lg:w-2/3">
         Our dedicated team works tirelessly to create a positive impact on society. Each member brings unique skills, commitment, and a shared vision to drive meaningful change.
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 "></div>
