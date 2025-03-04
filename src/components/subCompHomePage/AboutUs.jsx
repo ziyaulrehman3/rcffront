306f9a16e21928c2b5961f14ddca66b2e7b1c8e5
ziyaulrehman3 <ziyaulrehman3@gmail.com>
2025-03-05 02:20:43 +0530

    semi final 4.4

--- src/components/subCompHomePage/AboutUs.jsx
+++ src/components/subCompHomePage/AboutUs.jsx
@@ -4,7 +4,7 @@ import RightSide from "./RightSide";
 
 export default function AboutUs() {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center w-full max-w-[90%] md:max-w-[55%] h-auto lg:h-[750px] gap-0 m-auto mt-24">
+    <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-[90%] md:max-w-[55%] h-auto lg:h-[750px] gap-0 m-auto mt-24">
       <LeftSideAbout />
       <RightSide />
     </div>
