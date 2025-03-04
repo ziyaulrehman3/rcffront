b64c8bfe4fab9fe7231556f021a7ce4e97311cd7
ziyaulrehman3 <ziyaulrehman3@gmail.com>
2025-03-05 02:23:42 +0530

    semi final 4.5

--- src/components/Donation.jsx
+++ src/components/Donation.jsx
@@ -9,13 +9,13 @@ function Donation() {
 
   return (
     <div className="donation-page bg-[#D9D9D9] container mx-auto mt-8">
-      <div className="flex flex-col md:flex-row  gap-4">
+      <div className="flex flex-col md:flex-row gap-4">
         {/* Left column: Image occupying 50% */}
         <div className="w-full md:w-1/2">
           <img
             src="https://annamrita.org/wp-content/uploads/2023/02/5-ways-to-support-underprivileged-child-education.jpg"
             alt="Donation"
-            className="aspect-square  w-4/5 mx-auto h-auto object-cover bg-strokeyellow"
+            className="aspect-square  w-full mx-auto h-full object-cover"
           />
         </div>
         {/* Right column: Options occupying 50% */}
@@ -24,9 +24,9 @@ function Donation() {
           <div className="flex gap-4 mb-6">
             <button
               onClick={() => handleOptionChange("upi")}
-              className={`px-4 py-2 rounded-md ${
+              className={`px-6 py-2 rounded-md ${
                 selectedOption === "upi"
-                  ? "bg-blue-600 text-white"
+                  ? "bg-blue-500 text-white font-bold transition-all ease-linear"
                   : "bg-gray-300 text-black"
               }`}
             >
@@ -36,7 +36,7 @@ function Donation() {
               onClick={() => handleOptionChange("bank")}
               className={`px-4 py-2 rounded-md ${
                 selectedOption === "bank"
-                  ? "bg-blue-600 text-white"
+                  ? "bg-blue-500 text-white font-bold transition-all ease-linear"
                   : "bg-gray-300 text-black"
               }`}
             >
@@ -44,25 +44,33 @@ function Donation() {
             </button>
           </div>
 
-          <div className="w-full border rounded-md h-2/3">
+          <div className="w-full  rounded-md h-2/3">
             {selectedOption === "upi" ? (
-              <div className='h-full w-11/12'>
+              <div className="h-full w-11/12">
                 <h3 className="text-xl font-semibold mb-2">UPI Details:</h3>
-                <p className="text-base font-semibold">UPI ID: raaht89584648@barodampay</p><br/>
-                <img className="h-48 aspect-square" src="https://res.cloudinary.com/dqxjfn0qw/image/upload/v1740940676/qrcode_kmul7o.png" alt="qr-code" />
+                <p className="text-base font-semibold">
+                  UPI ID: raaht89584648@barodampay
+                </p>
+                <br />
+                <img
+                  className="h-48 aspect-square"
+                  src="https://res.cloudinary.com/dqxjfn0qw/image/upload/v1740940676/qrcode_kmul7o.png"
+                  alt="qr-code"
+                />
               </div>
             ) : (
-
               <div>
                 <h3 className="text-xl font-semibold mb-2">Bank Details:</h3>
                 <p className="text-base font-semibold">
-                  Bank Name: Bank of Baroda<br />
-                  Account Number: 05850200000648<br />
-                  IFSC: BARB0VJRAMP<br/>
+                  Bank Name: Bank of Baroda
+                  <br />
+                  Account Number: 05850200000648
+                  <br />
+                  IFSC: BARB0VJRAMP
+                  <br />
                   Branch: Rampur Branch
                 </p>
               </div>
-
             )}
           </div>
         </div>
@@ -70,11 +78,10 @@ function Donation() {
 
       <div className="my-4">
         <p className="font-poppins bg-slate-600 text-slate-50 text-center md:text-center w-full px-4 mx-auto">
-          Send your payments screenshot to get the Donation receipt on Our WhatsApp Number:
-          9876543210
+          Send your payments screenshot to get the Donation receipt on Our
+          WhatsApp Number: 9876543210
         </p>
       </div>
-
     </div>
   );
 }
