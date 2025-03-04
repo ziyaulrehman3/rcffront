c6744e1860c59ddfa0773324014e67f49dda8eac
ziyaulrehman3 <ziyaulrehman3@gmail.com>
2025-03-05 02:46:01 +0530

    semi final 4.5

--- src/components/subCompOurWork/JoinButtonComp.jsx
+++ src/components/subCompOurWork/JoinButtonComp.jsx
@@ -3,6 +3,6 @@ import {Link} from 'react-router-dom'
 export default function(){
 
     return (
-         <Link to='/Contact' className='bg-[#F0CF10] hover:bg-[#FFD900] text-black rounded-full flex w-28 h-10 text-sm font-bold justify-center items-center my-2'>Join Us</Link>
+         <Link to='/ContactUs' className='bg-[#F0CF10] hover:bg-[#FFD900] text-black rounded-full flex w-28 h-10 text-sm font-bold justify-center items-center my-2'>Join Us</Link>
     )
 }
